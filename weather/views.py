50e33026a631317d9e18ad282554117e9e106a29
Halina Nareika <gaikas9292@gmail.com>
2023-03-28 13:11:27 +0300

    Add url in index, weather in Pinsk

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = "6f10b3c18f9c94b32a2be17ef126a439"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = "Pinsk"
+    res = requests.get(url.format(city))
+
+    print(res.text)
     return render(request, 'weather/index.html')
