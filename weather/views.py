6d1e0a8605574b9a52aad490ea26d3ee6615b4ff
Halina Nareika <gaikas9292@gmail.com>
2023-03-28 15:14:00 +0300

    Add model City and change index

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,11 @@ def index(request):
     appid = "6f10b3c18f9c94b32a2be17ef126a439"
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
     city = "Pinsk"
-    res = requests.get(url.format(city))
-
-    print(res.text)
-    return render(request, 'weather/index.html')
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+    }
+    context = {'info': city_info}
+    return render(request, 'weather/index.html', context)
