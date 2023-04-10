b8a146e7e8dde6a1a1703f605d0470e7f5ff3ed8
Halina Nareika <gaikas9292@gmail.com>
2023-04-10 16:00:41 +0300

    Add message if gity is not exist

--- weather/models.py
+++ weather/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=30)
+    class Meta:
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
     def __str__(self):
         return self.name
--- weather/views.py
+++ weather/views.py
@@ -2,14 +2,36 @@ from django.shortcuts import render
 import requests
 from . models import City
 from . forms import CityForm
+from django.http import HttpResponse
 
 def index(request):
     appid = "c818b06e8114d649adf450a0a9ca213c"
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    err_msg = ''
+    message = ''
+    message_class = ''
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'Такого города не существует!'
+            else:
+                err_msg = 'Такой город уже добавлен!'
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'Город добавлен успешно'
+            message_class = 'is_success'
+
+
 
     form = CityForm()
 
@@ -27,7 +49,12 @@ def index(request):
             }
         all_cities.append(city_info)
 
-    context = {'all_info': all_cities, 'form': form}
+    context = {
+        'all_info': all_cities,
+        'form': form,
+        'message': message,
+        'message_class': message_class}
+
     return render(request, 'weather/index.html', context)
 
 def about(request):
